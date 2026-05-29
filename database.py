8d588045d1e9fc353c27b4eeaddd3f2617fdfaed
henriquemart <h.martins.silva2014@gmail.com>
2026-05-28 21:13:22 -0300

    Estrutura do projeto: completo

--- database.py
+++ database.py
@@ -5,6 +5,6 @@ mysql = MySQL()
 def init_db(app):
     app.config['MYSQL_HOST'] = 'localhost'
     app.config['MYSQL_USER'] = 'apiuser'
-    app.config['MYSQL_PASSWORD'] = 'senha123'
+    app.config['MYSQL_PASSWORD'] = '123'
     app.config['MYSQL_DB'] = 'minha_api'
     mysql.init_app(app)
